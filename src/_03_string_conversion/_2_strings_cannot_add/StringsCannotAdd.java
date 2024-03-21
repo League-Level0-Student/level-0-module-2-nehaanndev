fb80f4fef35aad777af64981af7c812e4ba8a9aa
neha <neha@192.168.50.224>
2024-03-20 18:28:13 -0700

    r

--- src/_03_string_conversion/_2_strings_cannot_add/StringsCannotAdd.java
+++ src/_03_string_conversion/_2_strings_cannot_add/StringsCannotAdd.java
@@ -24,17 +24,17 @@ public class StringsCannotAdd {
 			
 			// Insert fourAsString inside the parenthesis of Integer.parseInt() and set the method equal to fourAsInt
 			// Hint: int fourAsInt = Integer.parseInt(fourAsString);
-			
+			int fourAsInt = Integer.parseInt(fourAsString);
 			// 7) Do the same for sixAsString. 
-			
+			int sixAsInt = Integer.parseInt(sixAsString);
 			// 8) Print the addition of fourAsInt and sixAsInt using JOptionPane 
-			
+			JOptionPane.showMessageDialog(null, fourAsInt+sixAsInt);
 			// Did you notice the value 10? If so, hurray! You just changed the type of your variable from String to int.
 			
 			// 9) Convert fiveAsString to fiveAsInt using Integer.parseInt();  
-			
+			int fiveAsInt = Integer.parseInt(fiveAsString);
 			// 10) Print the addition of fourAsInt, sixAsInt, and fiveAsInt using JOptionPane 
-			
+			JOptionPane.showMessageDialog(null, fourAsInt+sixAsInt+fiveAsInt);
 			// Is your value now 15 instead of 465?
 		}
 }
--- src/_03_string_conversion/_3_rollercoaster/RollerCoaster.java
+++ src/_03_string_conversion/_3_rollercoaster/RollerCoaster.java
@@ -5,11 +5,16 @@ import javax.swing.JOptionPane;
 public class RollerCoaster {
 
 	// 1) Make a main method that includes all the steps below…. 
+		public static void main (String[] args ) {
+			
 		
 		// 2) Ask the user to enter their height in inches using JOptionPane and set it to the variable heightToRide
-		
+		String heightToRide=JOptionPane.showInputDialog("Enter your height in inches.");
+		int heightAsInt = Integer.parseInt(heightToRide);
 		// Uncomment the line below...
-			//if( heightToRide > 42 ) { }
+			if( heightAsInt > 42 ) { 
+				JOptionPane.showMessageDialog(null, "You are tall enough to ride the coaster alone!");
+			}
 		
 		// Do you see the heightToRide > 42 underlined red? That is because you are comparing a STRING to an INTEGER.
 		
@@ -17,9 +22,14 @@ public class RollerCoaster {
 	
 		// 4) If the heightAsInt is greater than or equal to 48 inches tall print:
 	    // "Hurray! You are tall enough to ride the coaster alone!" Hint: use JoptionPane
-		
+			else if( heightAsInt > 42 ) { 
+				JOptionPane.showMessageDialog(null, "You can ride the coaster with someone else!");
+			}
 		// 5) Else if the heightAsInt is greater than or equal to 42 inches tall print:
 		// "You can ride the coaster with someone else!" 
-	
+			else {
+				JOptionPane.showMessageDialog(null, "You must be at least 42 inches tall to ride the roller coaster pal!");
+			}
 		// 6) else print: "You must be at least 42 inches tall to ride the roller coaster pal!"
+		}
 } 
